2b1eb6f23de8d6274e649dc95bcb71cb97c9c569
Pablo PCVM <csr2453@outlook.es>
2023-04-28 14:16:17 -0400

    Configuracion del servidor y express

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+//Configuramos el servidor xpress
 const express = require('express');
 const app = express();
-const port = 3010;
+const port = 3000;
 
 //Definimos una ruta con /
-app.get('/', (reques, response) => {
+app.get('/', (request, response) => {
   response.send('Hola desde el server en expres');
 });
+
+//Nueva ruta
+app.get('/new-path', (request, response) => {
+  response.send('Esta es una nueva ruta');
+});
+
+//Ruta productos
+app.get('/products',(request,response)=>{
+  response.json(
+    [
+      {name:'Cellphone',price:7000},
+      {name:'Mouse',price:10},
+    ]
+  );
+});
+//Recuperamos un product especifico
+app.get( '/products/:id', (request, response) =>{
+  const id = request.params.id;
+  response.json(
+    {id, name:'Keyboard', price:22},
+  );
+} );
 
 //Le decimos a la app que escuche en un puerto específico
 app.listen(port, () => {
